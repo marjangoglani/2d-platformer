42b073e4c25c6fbde7c5dfc46e81b4946d5068d9
marjangoglani <91924395+marjangoglani@users.noreply.github.com>
2022-05-16 23:29:38 -0500

    game object components working, but the player script has a little error, haven't really started yet though so it's natural baby!

---
+++ Assets/enemy/enemy_movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// the enemy needs to walk left and right randomly and stop when it senses the player within its radius and
+// start shooting the player
+// when the player hits the enemy 5 itmes, then the enemy gets deleted and a new one gets generated from the
+// exact same initial location 
+ 
+
+
+public class enemy_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/player/player_movement.cs
+++ Assets/player/player_movement.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_movement : MonoBehaviour
-{
+// if you want to change the location of the player game object, then you need to get access to the transform 
+// component of the object
+
+public class player_movement : MonoBehaviour { 
+
+    public Transform transform;
+    public float speed = 10;
+    /*public float xPos;
+    public float yPos;*/
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*xPos = transform.position.x;
+        yPos = transform.position.y;   */               
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            transform.position.x += speed * Time.deltaTime;
+        }
     }
 }
