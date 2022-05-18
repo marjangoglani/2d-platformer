3390a8139fff0b1e8ad23cb1dac090c71ca42ef5
marjangoglani <91924395+marjangoglani@users.noreply.github.com>
2022-05-17 22:52:13 -0500

    crappy movement script

--- Assets/player/player_movement.cs
+++ Assets/player/player_movement.cs
@@ -8,23 +8,29 @@ using UnityEngine;
 public class player_movement : MonoBehaviour { 
 
     public Transform transform;
-    public float speed = 10;
-    /*public float xPos;
-    public float yPos;*/
+    public float speed = 15;
+    public float horizontal_movement;
+    public float vertical_movement;
+    public Vector2 movement_direction;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        /*xPos = transform.position.x;
-        yPos = transform.position.y;   */               
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Horizontal"))
-        {
-            transform.position.x += speed * Time.deltaTime;
-        }
+        horizontal_movement = Input.GetAxisRaw("Horizontal");
+        vertical_movement = Input.GetAxisRaw("Vertical");
+        movement_direction = new Vector3(horizontal_movement, vertical_movement, 0);
+        movement_direction.Normalize();
+        transform.Translate(movement_direction * speed * Time.deltaTime);
+
     }
+
+    // check the hor and ver movement in each update call and update animations accordingly. 
 }
